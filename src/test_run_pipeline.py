db8dc89f259c9b6714c99bd58c8f082a91fa6103
S. Skaugvoll <skaugvoll@gmail.com>
2019-02-16 14:23:04 +0100

    changed methods that are run

--- src/test_run_pipeline.py
+++ src/test_run_pipeline.py
@@ -269,15 +269,18 @@ dataframe_test.dropna(subset=['label'], inplace=True)
 ###############
 
 #### save the model so that the parallelized code can lode it for each new process
-s = input("save the trained RFC model? [y/n]: ")
-if s == 'y':
-    # TODO: fix where the file is saved
-    model_path = "./trained_rfc.sav"
-    pickle.dump(RFC, open(model_path, 'wb'))
-    print("MAX CPU CORES: ", os.cpu_count())
-    input("...")
-    p = Pipeline()
-    p.paralle_run(dataframe= dataframe_test, model_path=model_path, samples_pr_window=samples_pr_window, train_overlap=0.8)
+# s = input("save the trained RFC model? [y/n]: ")
+# if s == 'y':
+#     # TODO: fix where the file is saved
+#     model_path = "./trained_rfc.sav"
+#     pickle.dump(RFC, open(model_path, 'wb'))
+#     print("MAX CPU CORES: ", os.cpu_count())
+#     input("...")
+#     p = Pipeline()
+#     p.parallel_pipeline_classification_run(dataframe=dataframe_test, model_path=model_path, samples_pr_window=samples_pr_window, train_overlap=0.8)
+#
+#
+#
 
 
 
@@ -299,26 +302,21 @@ if s == 'y':
 
 
 
-
-
-
-###############
+##############
 # CLASSIFY (run without labels)
-###############
+##############
 
 # EXTRACT FEATURES
-# back_feat_test, thigh_feat_test, label_test = pipeObj.get_features_and_labels(dataframe_test)
+back_feat_test, thigh_feat_test, label_test = pipeObj.get_features_and_labels(dataframe_test)
 
-# res = RFC.classify(
-#     back_test_feat=back_feat_test,
-#     thigh_test_feat=thigh_feat_test,
-#     samples_pr_window=samples_pr_window,
-#     train_overlap=0.8
-# )
-#
-# print("CLASSIFICATION RESULT: \nSHAPE: {}\n{}: \n".format(res.shape, res))
-#
+res = RFC.classify(
+    back_test_feat=back_feat_test,
+    thigh_test_feat=thigh_feat_test,
+    samples_pr_window=samples_pr_window,
+    train_overlap=0.8
+)
 
+print("CLASSIFICATION RESULT: \nSHAPE: {}\n{}: \n".format(res.shape, res))
 
 
 
@@ -327,27 +325,29 @@ if s == 'y':
 
 
 
-# ##############
-# # TEST (GET ACCURACY)
-# ##############
-#
-# res = RFC.test(
-#     back_test_feat=back_feat_test,
-#     thigh_test_feat=thigh_feat_test,
-#     labels=label_test,
-#     samples_pr_window=samples_pr_window,
-#     train_overlap=0.8
-# )
-#
-# preds = RFC.predictions
-# ground_truth = RFC.test_ground_truth_labels
-# print("Calculating accuracy...")
-# acc = RFC.calculate_accuracy()
-# print("Done, ACC: {:.4f}".format(acc))
-#
-# print("Creating confusion matrix")
-# conf_mat = RFC.calculate_confusion_matrix()
-# print("DONE ")
-# print(conf_mat)
+
+
+##############
+# TEST (GET ACCURACY)
+##############
+
+res = RFC.test(
+    back_test_feat=back_feat_test,
+    thigh_test_feat=thigh_feat_test,
+    labels=label_test,
+    samples_pr_window=samples_pr_window,
+    train_overlap=0.8
+)
+
+preds = RFC.predictions
+ground_truth = RFC.test_ground_truth_labels
+print("Calculating accuracy...")
+acc = RFC.calculate_accuracy()
+print("Done, ACC: {:.4f}".format(acc))
+
+print("Creating confusion matrix")
+conf_mat = RFC.calculate_confusion_matrix()
+print("DONE ")
+print(conf_mat)
 
 
