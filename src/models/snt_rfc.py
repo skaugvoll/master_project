3bb0cb1f88075e4ea6478c663e20eca6b8d7e624
S. Skaugvoll <skaugvoll@gmail.com>
2019-02-16 14:21:16 +0100

    Extracted the temperature feature and segmentation methods into its own script in utils packaged. And imports it in Pipeline.py and snt_rfc.py, reduced filesize and code duplication.

--- src/models/snt_rfc.py
+++ src/models/snt_rfc.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import numpy as np
-from collections import Counter
+import utils.temperature_segmentation_and_calculation as temp_feature_util
+# from collections import Counter
 from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.metrics import accuracy_score, confusion_matrix
 
@@ -15,127 +16,6 @@ class HARRandomForrest():
         self.accuracy = None
         self.confusion_matrix = None
 
-    def max(self, array):
-        '''
-        Finds and returns the highest value in the array
-        :param array:
-        :return:
-        '''
-        return np.max(array)
-
-    def min(self, array):
-        '''
-        Finds and returns the lowest value in the array
-        :param array:
-        :return:
-        '''
-        return np.min(array)
-
-    def first_last_delta(self, array):
-        temperature_first_sample_in_window = array[0]
-        temperature_last_sample_in_window = array[-1]
-        return (temperature_last_sample_in_window - temperature_first_sample_in_window)
-
-    def max_min_delta(self, array):
-        '''
-        # The variation in temperature from the warmest to the coldest
-        :param array:
-        :return:
-        '''
-        #max_temp = max(array)
-        #min_temp = min(array)
-
-        max_temp = np.amax(array)
-        min_temp = np.amin(array)
-        return (max_temp - min_temp)
-
-
-    def segment_acceleration_and_calculate_features(self,
-                                                    sensor_data,
-                                                    samples_pr_window=50,
-                                                    overlap=0.0,
-                                                    remove_sign_after_calculation=True):
-        '''
-
-        :param sensor_data:
-        :param samples_pr_window:
-        :param overlap:
-        :param remove_sign_after_calculation:
-        :return:
-        '''
-
-        # print("len sensor data: ", sensor_data.shape)
-        functions = [
-            self.max,
-            self.min,
-            self.max_min_delta,
-            self.first_last_delta,
-        ]
-
-        # window_samples = int(sampling_rate * window_length)
-        window_samples = samples_pr_window
-
-        # print("Windows samples ", window_samples)
-        step_size = int(round(window_samples * (1.0 - overlap)))
-
-        all_features = []
-
-        for window_start in np.arange(0, sensor_data.shape[0], step_size):
-            # print("Window start: ", window_start, "Sensor_data.shape[0]: ", sensor_data.shape[0], step_size)
-            window_start = int(round(window_start))
-            window_end = window_start + int(round(window_samples))
-            if window_end > sensor_data.shape[0]:
-                break
-            window = sensor_data[window_start:window_end]
-
-            # print("Window", window)
-            extracted_features = []
-            # print("Windows start: ")
-            index_of_function = 0
-            n = 0
-            for func in functions:
-                value = func(window)
-                extracted_features.append(value)
-
-            all_features.append(np.hstack(extracted_features))
-
-        one_large_array = np.vstack(all_features)
-
-        if remove_sign_after_calculation:
-            np.absolute(one_large_array, one_large_array)
-
-        print(one_large_array.shape)
-
-        return one_large_array
-
-    def segment_labels(self, label_data, overlap=0.0, samples_pr_window = 50):
-        # window_samples = int(sampling_rate * window_length)
-        window_samples = samples_pr_window
-        step_size = int(round(window_samples * (1.0 - overlap)))
-
-        labels = []
-
-        for window_start in np.arange(0, label_data.shape[0], step_size):
-            window_start = int(round(window_start))
-            window_end = window_start + int(round(window_samples))
-            if window_end > label_data.shape[0]:
-                break
-            window = label_data[window_start:window_end]
-            # print(window)
-            top = self.find_majority_activity(window)
-            labels.append(top)
-
-        return np.array(labels)
-
-    def find_majority_activity(self, window):
-        sensor_labels_list = window.tolist()
-        labels_without_list = []
-        for sensor_label in sensor_labels_list:
-            labels_without_list.append(sensor_label[0])
-        counts = Counter(labels_without_list)
-        top = counts.most_common(1)[0][0]
-        return top
-
     def train(self,
               back_training_feat,
               thigh_training_feat,
@@ -147,15 +27,15 @@ class HARRandomForrest():
 
             print("RFC TRAIN BTF: ", back_training_feat)
 
-            back_training_feat = self.segment_acceleration_and_calculate_features(back_training_feat,
+            back_training_feat = temp_feature_util.segment_acceleration_and_calculate_features(back_training_feat,
                                                                                   samples_pr_window=samples_pr_window,
                                                                                   overlap=train_overlap)
 
-            thigh_training_feat = self.segment_acceleration_and_calculate_features(thigh_training_feat,
+            thigh_training_feat = temp_feature_util.segment_acceleration_and_calculate_features(thigh_training_feat,
                                                                                     samples_pr_window=samples_pr_window,
                                                                                     overlap=train_overlap)
 
-            labels = self.segment_labels(labels, samples_pr_window=samples_pr_window, overlap=train_overlap)
+            labels = temp_feature_util.segment_labels(labels, samples_pr_window=samples_pr_window, overlap=train_overlap)
            
             both_features = np.hstack((back_training_feat, thigh_training_feat))
 
@@ -171,15 +51,15 @@ class HARRandomForrest():
 
         print("RFC TEST BTF: ", back_test_feat)
 
-        back_test_feat = self.segment_acceleration_and_calculate_features(back_test_feat,
+        back_test_feat = temp_feature_util.segment_acceleration_and_calculate_features(back_test_feat,
                                                                           samples_pr_window=samples_pr_window,
                                                                           overlap=train_overlap)
 
-        thigh_test_feat = self.segment_acceleration_and_calculate_features(thigh_test_feat,
+        thigh_test_feat = temp_feature_util.segment_acceleration_and_calculate_features(thigh_test_feat,
                                                                            samples_pr_window=samples_pr_window,
                                                                            overlap=train_overlap)
 
-        self.test_ground_truth_labels = self.segment_labels(labels, samples_pr_window=samples_pr_window, overlap=train_overlap)
+        self.test_ground_truth_labels = temp_feature_util.segment_labels(labels, samples_pr_window=samples_pr_window, overlap=train_overlap)
 
         both_features = np.hstack((back_test_feat, thigh_test_feat))
 
@@ -189,11 +69,11 @@ class HARRandomForrest():
         print("PREDICTIONS: \n{}".format(self.predictions))
 
     def classify(self, back_test_feat, thigh_test_feat, samples_pr_window, train_overlap):
-        back_test_feat = self.segment_acceleration_and_calculate_features(back_test_feat,
+        back_test_feat = temp_feature_util.segment_acceleration_and_calculate_features(back_test_feat,
                                                                           samples_pr_window=samples_pr_window,
                                                                           overlap=train_overlap)
 
-        thigh_test_feat = self.segment_acceleration_and_calculate_features(thigh_test_feat,
+        thigh_test_feat = temp_feature_util.segment_acceleration_and_calculate_features(thigh_test_feat,
                                                                            samples_pr_window=samples_pr_window,
                                                                            overlap=train_overlap)
         both_features = np.hstack((back_test_feat, thigh_test_feat))
@@ -221,33 +101,3 @@ class HARRandomForrest():
         res = self.RFC_classifier.predict([window])
         return res
 
-    # def classifyWindowByWindow(self, back_test_feat, thigh_test_feat, samples_pr_window, train_overlap):
-    #     back_test_feat = self.segment_acceleration_and_calculate_features(back_test_feat,
-    #                                                                       samples_pr_window=samples_pr_window,
-    #                                                                       overlap=train_overlap)
-    #
-    #     thigh_test_feat = self.segment_acceleration_and_calculate_features(thigh_test_feat,
-    #                                                                        samples_pr_window=samples_pr_window,
-    #                                                                        overlap=train_overlap)
-    #     both_features = np.hstack((back_test_feat, thigh_test_feat))
-    #
-    #     print("BOTH FEATURES: \n{}".format(both_features))
-    #
-    #     print("both_features [0] \n{}".format(both_features[0]))
-    #
-    #     print("Classifying 10 first windows\n")
-    #     i = 0
-    #     for i, w in enumerate(both_features):
-    #         res = self.RFC_classifier.predict([both_features[i]])
-    #         print(i, " :: ", w, " --> ", res)
-    #         i += 1
-    #         if i == 10:
-    #             break
-    #
-    #     # jobs = []
-    #     # num_processes = 10
-    #     # for i in range(num_processes):
-    #     #     process = multiprocessing.Process(target=self.window_classification,
-    #     #                                       args=(w))
-    #     #     jobs.append(process)
-    #
--- src/pipeline/Pipeline.py
+++ src/pipeline/Pipeline.py
@@ -11,6 +11,7 @@ from collections import Counter
 from multiprocessing import Process, Queue, current_process, freeze_support
 from pipeline.DataHandler import DataHandler
 from src import models
+import utils.temperature_segmentation_and_calculation as temp_feature_util
 
 class Pipeline:
     def __init__(self):
@@ -171,145 +172,10 @@ class Pipeline:
     #                                   vPARALLELL PIPELINE EXECUTE WINDOW BY WINDOW CODEv                             #
     ####################################################################################################################
 
-    def max(self, array):
-        '''
-        Finds and returns the highest value in the array
-        :param array:
-        :return:
-        '''
-        return np.max(array)
-
-    def min(self, array):
-        '''
-        Finds and returns the lowest value in the array
-        :param array:
-        :return:
-        '''
-        return np.min(array)
-
-    def first_last_delta(self, array):
-        temperature_first_sample_in_window = array[0]
-        temperature_last_sample_in_window = array[-1]
-        return (temperature_last_sample_in_window - temperature_first_sample_in_window)
-
-    def max_min_delta(self, array):
-        '''
-        # The variation in temperature from the warmest to the coldest
-        :param array:
-        :return:
-        '''
-        # max_temp = max(array)
-        # min_temp = min(array)
-
-        max_temp = np.amax(array)
-        min_temp = np.amin(array)
-        return (max_temp - min_temp)
-
-    def segment_acceleration_and_calculate_features(self,
-                                                    sensor_data,
-                                                    samples_pr_window=50,
-                                                    overlap=0.0,
-                                                    remove_sign_after_calculation=True):
-        '''
-
-        :param sensor_data:
-        :param samples_pr_window:
-        :param overlap:
-        :param remove_sign_after_calculation:
-        :return:
-        '''
-
-        # print("len sensor data: ", sensor_data.shape)
-        functions = [
-            self.max,
-            self.min,
-            self.max_min_delta,
-            self.first_last_delta,
-        ]
-
-        # window_samples = int(sampling_rate * window_length)
-        window_samples = samples_pr_window
-
-        # print("Windows samples ", window_samples)
-        step_size = int(round(window_samples * (1.0 - overlap)))
-
-        all_features = []
-
-        for window_start in np.arange(0, sensor_data.shape[0], step_size):
-            # print("Window start: ", window_start, "Sensor_data.shape[0]: ", sensor_data.shape[0], step_size)
-            window_start = int(round(window_start))
-            window_end = window_start + int(round(window_samples))
-            if window_end > sensor_data.shape[0]:
-                break
-            window = sensor_data[window_start:window_end]
-
-            # print("Window", window)
-            extracted_features = []
-            # print("Windows start: ")
-            index_of_function = 0
-            n = 0
-            for func in functions:
-                value = func(window)
-                extracted_features.append(value)
-
-            all_features.append(np.hstack(extracted_features))
-
-        one_large_array = np.vstack(all_features)
-
-        if remove_sign_after_calculation:
-            np.absolute(one_large_array, one_large_array)
-
-        print(one_large_array.shape)
-
-        return one_large_array
-
-    def segment_labels(self, label_data, overlap=0.0, samples_pr_window=50):
-        # window_samples = int(sampling_rate * window_length)
-        window_samples = samples_pr_window
-        step_size = int(round(window_samples * (1.0 - overlap)))
-
-        labels = []
-
-        for window_start in np.arange(0, label_data.shape[0], step_size):
-            window_start = int(round(window_start))
-            window_end = window_start + int(round(window_samples))
-            if window_end > label_data.shape[0]:
-                break
-            window = label_data[window_start:window_end]
-            # print(window)
-            top = self.find_majority_activity(window)
-            labels.append(top)
-
-        return np.array(labels)
-
-    def find_majority_activity(self, window):
-        sensor_labels_list = window.tolist()
-        labels_without_list = []
-        for sensor_label in sensor_labels_list:
-            labels_without_list.append(sensor_label[0])
-        counts = Counter(labels_without_list)
-        top = counts.most_common(1)[0][0]
-        return top
-
-
-    ####################################################################################################################
-    #                                   ^PARALLELL PIPELINE EXECUTE WINDOW BY WINDOW CODE^                             #
-    ####################################################################################################################
-
-
     # MODEL Klassifisering
     def model_classification_worker(self, input_q, output, model):
         for window in iter(input_q.get, 'STOP'):
             print("model_classification_worker executing")
-            # if window[0] <= 5:
-            #     res = (1, window)
-            #     time.sleep(5.5)
-            # elif window[0] <= 13:
-            #     res = (2, window)
-            #     time.sleep(2.5)
-            # else:
-            #     res = (3, window)
-            #     time.sleep(0.05)
 
             # Vil ha in ett ferdig window, aka windows maa lages utenfor her og addes til queue
             res = model.window_classification(window)
@@ -327,7 +193,7 @@ class Pipeline:
             print("MODEL: {}\nWINDOW: {}".format(model, w))
 
 
-    def paralle_run(self, dataframe, model_path, samples_pr_window, train_overlap):
+    def parallel_pipeline_classification_run(self, dataframe, model_path, samples_pr_window, train_overlap):
         NUMBER_OF_PROCESSES_models = 3
         NUMBER_OF_PROCESSES_class = 1
 
@@ -337,11 +203,11 @@ class Pipeline:
 
         # Submit tasks for model klassifisering
         back_feat, thigh_feat, label = self.get_features_and_labels(dataframe) # returns numpy arrays
-        back_feat = self.segment_acceleration_and_calculate_features(back_feat,
+        back_feat = temp_feature_util.segment_acceleration_and_calculate_features(back_feat,
                                                                     samples_pr_window=samples_pr_window,
                                                                     overlap=train_overlap)
 
-        thigh_feat = self.segment_acceleration_and_calculate_features(thigh_feat,
+        thigh_feat = temp_feature_util.segment_acceleration_and_calculate_features(thigh_feat,
                                                                     samples_pr_window=samples_pr_window,
                                                                     overlap=train_overlap)
         both_features = np.hstack((back_feat, thigh_feat))
