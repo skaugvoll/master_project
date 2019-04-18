9da60a9443222e315299421056622019695aeba0
Woldt <thmwld@gmail.com>
2019-04-18 16:29:03 +0200

    Leave one out code example i lstm both

--- src/train_lstm_both.py
+++ src/train_lstm_both.py
@@ -52,3 +52,15 @@ res = pipObj.evaluate_lstm_model(
 )
 
 print("Evaluation result: {}".format(res))
+
+#LEAVE ONE OUT X_VAL
+# import numpy as np
+# from sklearn.model_selection import LeaveOneOut
+# X = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16], [17, 18], [19, 20]])
+#
+# loo = LeaveOneOut()
+#
+# for train_index, test_index in loo.split(X):
+#   print("TRAIN:", train_index, "TEST:", test_index)
+#   X_train, X_test = X[train_index], X[test_index]
+#   print(X_train, X_test)
